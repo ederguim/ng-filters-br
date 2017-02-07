58eba041a216d279ba7c27d25bb61c177c13e324
eder.morais <eder.morais@centralit.com.br>
2017-02-07 16:17:45 -0200

    Inserido filtro de codigo de barras

--- dist/ng-filters-br.js
+++ dist/ng-filters-br.js
@@ -115,3 +115,21 @@ angular.module('brasil.filters').filter('cpfCnpj', ['$filter', function ($filter
         return str;
     };
 }]);
+
+// Source: dist/.temp/brasil/filters/codigoBarra.js
+angular.module('brasil.filters').filter('codigoBarra', function() {
+    return function (input) {
+        var str = input + '';
+        str=str.replace(/^(\d{4})(\d)/,'$1 $2');
+        str=str.replace(/^(\d{4})\s(\d{4})(\d)/,'$1 $2 $3')
+        str=str.replace(/^(\d{4})\s(\d{4})\s(\d{4})(\d)/,'$1 $2 $3 $4')
+        str=str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/,'$1 $2 $3 $4 $5')
+        str=str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/,'$1 $2 $3 $4 $5 $6')
+        str=str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/,'$1 $2 $3 $4 $5 $6 $7')
+        str=str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/,'$1 $2 $3 $4 $5 $6 $7 $8')
+        str=str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/,'$1 $2 $3 $4 $5 $6 $7 $8 $9')
+        str=str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/,'$1 $2 $3 $4 $5 $6 $7 $8 $9 $10')
+        str=str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/,'$1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11')
+        return str;
+    };
+});
