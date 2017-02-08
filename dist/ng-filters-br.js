c2130990e7c4f398b1aaa0afae19847f9860b574
eder.morais <eder.morais@centralit.com.br>
2017-02-08 17:06:13 -0200

    Correção no filtro de codigo de barras

--- dist/ng-filters-br.js
+++ dist/ng-filters-br.js
@@ -35,16 +35,19 @@ angular.module('brasil.filters').filter('cnpj', function () {
 angular.module('brasil.filters').filter('codigoBarra', function () {
   return function (input) {
     var str = input + '';
-    str = str.replace(/^(\d{4})(\d)/, '$1 $2');
-    str = str.replace(/^(\d{4})\s(\d{4})(\d)/, '$1 $2 $3');
-    str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4');
-    str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5');
-    str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6');
-    str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6 $7');
-    str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6 $7 $8');
-    str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6 $7 $8 $9');
-    str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6 $7 $8 $9 $10');
-    str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11');
+    if (str.length === 44) {
+      str = str.replace(/^(\d{4})(\d)/, '$1 $2');
+      str = str.replace(/^(\d{4})\s(\d{4})(\d)/, '$1 $2 $3');
+      str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4');
+      str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5');
+      str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6');
+      str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6 $7');
+      str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6 $7 $8');
+      str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6 $7 $8 $9');
+      str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6 $7 $8 $9 $10');
+      str = str.replace(/^(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})\s(\d{4})(\d)/, '$1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11');
+      return str;
+    }
     return str;
   };
 });
